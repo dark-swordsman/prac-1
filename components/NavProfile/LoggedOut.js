15b6a66ab65bb973cc6e7a3163dfb4132ee10c07
dark-swordsman <kylebarr54321@gmail.com>
2021-04-19 19:14:06 -0600

    remove focus ring on navprofile buttons

--- components/NavProfile/LoggedOut.js
+++ components/NavProfile/LoggedOut.js
@@ -2,10 +2,10 @@ function LoggedOut() {
   return (
     <div className="flex space-x-2">
       <div className="w-24 h-9">
-        <button className="w-full h-full rounded-lg bg-blue-700 hover:bg-blue-800 hover:text-gray-300">Sign Up</button>
+        <button className="w-full h-full rounded-lg focus:outline-none bg-blue-700 hover:bg-blue-800 hover:text-gray-300">Sign Up</button>
       </div>
       <div className="w-24 h-9">
-        <button className="w-full h-full rounded-lg border-2 text-gray-300 border-gray-300 hover:text-gray-400 hover:border-gray-400">Log In</button>
+        <button className="w-full h-full rounded-lg border-2 focus:outline-none text-gray-300 border-gray-300 hover:text-gray-400 hover:border-gray-400">Log In</button>
       </div>
     </div>
   );
