72e6c0f18966c0dcbaf7c6dd3c3b77bf14a2033f
dark-swordsman <kylebarr54321@gmail.com>
2021-04-19 22:20:00 -0600

    add flex divs to anime page

--- pages/anime/[id].js
+++ pages/anime/[id].js
@@ -10,12 +10,20 @@ function Anime() {
   const router = useRouter();
   const { id } = router.query;
 
+  const data = [
+    "aaaaa",
+    "bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb bbbbbbbbbbb bbbb "
+  ];
+
   return (
     <Layout>
       <CustomHead title={id} />
       <Header />
 
-      {id}
+      <div className="flex">
+        <div className="w-56 md:w-64 bg-pink-500">{data[0]}</div>
+        <div className="flex-1 bg-blue-500">{data[1]}</div>
+      </div>
     </Layout>
   );
 }
