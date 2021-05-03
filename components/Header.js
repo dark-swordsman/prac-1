dd0a15a2b9af30b9bd1b63dc4f9b5bb7914d2a07
dark-swordsman <kylebarr54321@gmail.com>
2021-05-03 17:43:50 -0600

    fix 'class' warning on Header component

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import NavProfile from './NavProfile/NavProfile';
 
 function Header() {
 	return (
-		<div class="h-12 flex justify-between items-center">
+		<div className="h-12 flex justify-between items-center">
 			<img src="/placeholder-neighbor.png" />
 			<NavProfile />
 		</div>
