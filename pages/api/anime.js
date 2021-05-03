7f1d7d95565dc9ede01ceeae779dff8a946006d0
dark-swordsman <kylebarr54321@gmail.com>
2021-05-03 14:41:18 -0600

    add anime data to anime api endpoint

--- pages/api/anime.js
+++ pages/api/anime.js
@@ -1,3 +1,882 @@
+// TODO: Possibly pull in data generation script to emulate SWR?
+// const fs = require('fs');
+let animeData = {
+	"anime": [
+		{
+			"name": "Gintama",
+			"genres": [
+				"Horror"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Thriller",
+				"Comedy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Seinen"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"Romance"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "3-gatsu no Lion 2nd Season",
+			"genres": [
+				"Adventure",
+				"Shoujo"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"Sports",
+				"Action"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Clannad",
+			"genres": [
+				"Sports",
+				"Sci-Fi"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Ecchi",
+				"Horror"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Action"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Action",
+				"Drama"
+			],
+			"type": "Movie",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Comedy",
+				"Seinen"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Music"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Fantasy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Psychological",
+				"Thriller"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "3-gatsu no Lion 2nd Season",
+			"genres": [
+				"Mecha",
+				"Fantasy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Psychological"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Shounen",
+				"Ecchi"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Shingeki no Kyojin Season 3 Part 2",
+			"genres": [
+				"Game",
+				"Drama"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Seinen",
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Mecha"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Drama"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Action",
+				"Fantasy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Shingeki no Kyojin Season 3 Part 2",
+			"genres": [
+				"Slice of Life"
+			],
+			"type": "Movie",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "3-gatsu no Lion 2nd Season",
+			"genres": [
+				"Mecha"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Music"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Fantasy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Shingeki no Kyojin Season 3 Part 2",
+			"genres": [
+				"Yaoi"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Shounen",
+				"Drama"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Ecchi"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Psychological"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "3-gatsu no Lion 2nd Season",
+			"genres": [
+				"Action",
+				"Seinen"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Sci-Fi",
+				"Action"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Clannad: After Story",
+			"genres": [
+				"Yaoi",
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"School"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "3-gatsu no Lion 2nd Season",
+			"genres": [
+				"Slice of Life",
+				"Game"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"Game"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Ecchi",
+				"Sports"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Yaoi",
+				"Adventure"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Romance"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "3-gatsu no Lion 2nd Season",
+			"genres": [
+				"Music",
+				"Ecchi"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Clannad",
+			"genres": [
+				"Thriller"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Romance",
+				"Shounen"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Sports",
+				"Action"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Horror",
+				"Sports"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Adventure",
+				"Sci-Fi"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"Horror"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Fantasy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Psychological",
+				"Action"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Mystery",
+				"Thriller"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Mystery"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"Horror",
+				"Mecha"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"Music",
+				"Fantasy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Fantasy",
+				"Yaoi"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Shoujo",
+				"Action"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Romance",
+				"Thriller"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Action",
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Clannad: After Story",
+			"genres": [
+				"Seinen"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Fantasy",
+				"Drama"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Shingeki no Kyojin Season 3 Part 2",
+			"genres": [
+				"Yaoi"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Mecha"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Shoujo",
+				"Slice of Life"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Shounen"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Thriller"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Fantasy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Sports",
+				"Adventure"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Clannad",
+			"genres": [
+				"Music",
+				"Seinen"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Shounen"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Clannad",
+			"genres": [
+				"Fantasy",
+				"School"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Mecha"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Shoujo"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Adventure",
+				"Shoujo"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Adventure",
+				"School"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Clannad",
+			"genres": [
+				"Ecchi",
+				"Adventure"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Action"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"School"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Psychological"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Shoujo"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Mystery"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Gintama.",
+			"genres": [
+				"Romance",
+				"Comedy"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Clannad: After Story",
+			"genres": [
+				"Action"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Psychological"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Hunter x Hunter (2011)",
+			"genres": [
+				"Action",
+				"Ecchi"
+			],
+			"type": "OVA",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "3-gatsu no Lion 2nd Season",
+			"genres": [
+				"Sci-Fi",
+				"School"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Game",
+				"Sci-Fi"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-6.png"
+		},
+		{
+			"name": "Koe no Katachi",
+			"genres": [
+				"Horror",
+				"Drama"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Mecha"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Shingeki no Kyojin Season 3 Part 2",
+			"genres": [
+				"Adventure",
+				"Thriller"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Psychological",
+				"Mystery"
+			],
+			"type": "Movie",
+			"image": "/img/poster-lg-7.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Comedy",
+				"Drama"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-4.png"
+		},
+		{
+			"name": "Clannad: After Story",
+			"genres": [
+				"Sports"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Steins;Gate",
+			"genres": [
+				"Psychological",
+				"Game"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-1.png"
+		},
+		{
+			"name": "Shingeki no Kyojin Season 3 Part 2",
+			"genres": [
+				"Slice of Life",
+				"Mecha"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-2.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Shounen",
+				"Sports"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-0.png"
+		},
+		{
+			"name": "Gintama",
+			"genres": [
+				"Music"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		},
+		{
+			"name": "Ginga Eiyuu Densetsu",
+			"genres": [
+				"Drama",
+				"Adventure"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-3.png"
+		},
+		{
+			"name": "Fullmetal Alchemist: Brotherhood",
+			"genres": [
+				"Music",
+				"Shoujo"
+			],
+			"type": "TV",
+			"image": "/img/poster-lg-5.png"
+		}
+	]
+};
+
+// async function getAnimeData() {
+//   const dataDirectory = '../../scripts/dataDump/';
+//   let animeDataFilename = null;
+//   fs.readdir(dataDirectory, (err, files) => {
+//     animeDataFilename = files[0];
+
+//     if (err) console.error(err);
+//   });
+
+//   return JSON.parse(fs.readFileSync(dataDirectory + animeDataFilename));
+// }
+
 export default function handler(req, res) {
-  res.status(200).json({ key: "value" });
+  if (!animeData) {
+    getAnimeData().then((data) => {
+      res.status(200).json(data);
+    });
+  } else {
+    res.status(200).json(animeData);
+  }
 }
