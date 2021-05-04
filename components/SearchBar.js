692ac9cf037c135f738a9ba64573a2886fd058ec
dark-swordsman <kylebarr54321@gmail.com>
2021-05-04 12:25:06 -0600

    add search button to SearchBar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -11,7 +11,8 @@ function SearchBar() {
 
   return (
     <div className="w-100 flex justify-center py-16">
-      <input ref={wrapperRef} type="text" onClick={() => setIsFocused(!isFocused)} placeholder="Search for anime..." className={(isFocused ? "cursor-text" : "cursor-pointer") + " duration-75 text-2xl w-3/4 h-14 rounded-xl px-6 outline-none placeholder-gray-400 bg-gray-700 focus:placeholder-opacity-50 focus:bg-gray-800"} />
+      <input ref={wrapperRef} type="text" onClick={() => setIsFocused(!isFocused)} placeholder="Search for anime..." className={(isFocused ? "cursor-text" : "cursor-pointer") + " duration-75 text-2xl w-2/3 h-14 rounded-l-xl px-6 outline-none placeholder-gray-400 bg-gray-700 focus:placeholder-opacity-50 focus:bg-gray-800"} />
+      <button className="bg-blue-700 hover:bg-blue-800 active:bg-blue-900 focus:outline-none px-4 text-xl rounded-r-xl select-none">Search</button>
     </div>
   );
 }
