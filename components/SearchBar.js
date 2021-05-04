675c924dffe429c214c0da2c78f230fbb726595b
dark-swordsman <kylebarr54321@gmail.com>
2021-05-04 13:41:17 -0600

    change SearchBar button to green

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -15,7 +15,7 @@ function SearchBar() {
   return (
     <div className="w-100 flex justify-center py-16">
       <input ref={wrapperRef} type="text" onClick={() => { if (!isFocused) setIsFocused(true) } } placeholder="Search for anime..." className={(isFocused ? "cursor-text" : "cursor-pointer") + " duration-75 text-2xl w-2/3 h-14 rounded-l-xl px-6 outline-none placeholder-gray-400 bg-gray-700 focus:placeholder-opacity-50 focus:bg-gray-800"} />
-      <button className="bg-blue-700 hover:bg-blue-800 active:bg-blue-900 focus:outline-none px-4 text-xl rounded-r-xl select-none">Search</button>
+      <button className="bg-green-700 hover:bg-green-800 active:bg-green-900 focus:outline-none px-4 text-xl rounded-r-xl select-none">Search</button>
     </div>
   );
 }
