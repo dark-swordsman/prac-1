206d38613875e350bd0d425d3335f8a3a95e28bd
dark-swordsman <kylebarr54321@gmail.com>
2021-05-03 18:24:07 -0600

    add search page

---
+++ pages/search.js
@@ -0,0 +1,19 @@
+import {
+  CustomHead,
+  Header,
+  Layout,
+} from "../components";
+
+function Search() {
+  return (
+    <Layout>
+      <CustomHead title="Search" />
+      <Header />
+
+      yep. search things
+      
+    </Layout>
+  )
+}
+
+export default Search;
