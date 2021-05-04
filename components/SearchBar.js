c349f86e96e4fb2d394b79aba8ca647fede1e893
dark-swordsman <kylebarr54321@gmail.com>
2021-05-04 12:27:31 -0600

    fixed SearchBar changing cursor on every click, and unnecessary state changes

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -4,6 +4,9 @@ import { useOutsideAlerter } from '../helpers';
 function SearchBar() {
   // support multiple modes (like spotlight-style, medium, small, etc.)
   // default mode for now will be wide
+
+  // TODO: create search functionality.
+  // TODO: enable search on Enter key press.
   const [isFocused, setIsFocused] = useState(false);
 
   const wrapperRef = useRef(null);
@@ -11,7 +14,7 @@ function SearchBar() {
 
   return (
     <div className="w-100 flex justify-center py-16">
-      <input ref={wrapperRef} type="text" onClick={() => setIsFocused(!isFocused)} placeholder="Search for anime..." className={(isFocused ? "cursor-text" : "cursor-pointer") + " duration-75 text-2xl w-2/3 h-14 rounded-l-xl px-6 outline-none placeholder-gray-400 bg-gray-700 focus:placeholder-opacity-50 focus:bg-gray-800"} />
+      <input ref={wrapperRef} type="text" onClick={() => { if (!isFocused) setIsFocused(true) } } placeholder="Search for anime..." className={(isFocused ? "cursor-text" : "cursor-pointer") + " duration-75 text-2xl w-2/3 h-14 rounded-l-xl px-6 outline-none placeholder-gray-400 bg-gray-700 focus:placeholder-opacity-50 focus:bg-gray-800"} />
       <button className="bg-blue-700 hover:bg-blue-800 active:bg-blue-900 focus:outline-none px-4 text-xl rounded-r-xl select-none">Search</button>
     </div>
   );
